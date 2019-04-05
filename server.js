eb9b672605412d868b071efe778446a78879293e
andyols <andyolson09@gmail.com>
2019-04-05 18:01:23 -0500

    express server route setup

--- server.js
+++ server.js
@@ -11,8 +11,13 @@ mongoose
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err))
 
-// Test Route
-app.get('/', (req, res) => res.send('Hello world'))
+// Connect route files to server
+const users = require('./routes/api/users')
+const profile = require('./routes/api/profile')
+const posts = require('./routes/api/posts')
+app.use('/api/users', users)
+app.use('/api/profile', profile)
+app.use('/api/posts', posts)
 
 const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`Server running on port ${port}`))
