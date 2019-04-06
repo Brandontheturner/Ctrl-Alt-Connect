aa9e7861bfd168911872a7b773cbb50c127b8719
andyols <andyolson09@gmail.com>
2019-04-06 18:06:07 -0500

    Like / Unlike, Comment / Uncomment Post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -5,6 +5,7 @@ const passport = require('passport')
 
 // Load Input Validation
 const valiidatePostInput = require('../../validation/post')
+const validateCommentInput = require('../../validation/comment')
 // Load Post / Profile Model
 const Post = require('../../models/Post')
 const Profile = require('../../models/Profile')
@@ -16,7 +17,7 @@ const Profile = require('../../models/Profile')
  */
 router.get('/test', (req, res) => res.json({ msg: 'Posts test route working' }))
 
-/***********************************
+/*******************************
  *  @route  -->  POST api/posts
  *  @desc   -->  Create a post
  *  @access -->  Private
@@ -94,6 +95,139 @@ router.delete(
         )
     })
   ]
+)
+
+/****************************************
+ *  @route  -->  POST api/posts/like/:id
+ *  @desc   -->  Like a post
+ *  @access -->  Private
+ */
+router.post(
+  '/like/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => [
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          // Ensure user has not already liked this post
+          if (
+            post.likes.filter(item => item.user.toString() === req.user.id)
+              .length > 0
+          ) {
+            return res
+              .status(400)
+              .json({ alreadyliked: 'User has already liked this post' })
+          }
+
+          post.likes.unshift({ user: req.user.id })
+          post.save().then(post => res.json(post))
+        })
+        .catch(err =>
+          res.status(404).json({ nopostfound: 'No post found with that id' })
+        )
+    })
+  ]
+)
+
+/******************************************
+ *  @route  -->  POST api/posts/unlike/:id
+ *  @desc   -->  Remove a like
+ *  @access -->  Private
+ */
+router.post(
+  '/unlike/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          // Ensure user has not already liked this post
+          if (
+            post.likes.filter(item => item.user.toString() === req.user.id)
+              .length === 0
+          ) {
+            return res
+              .status(400)
+              .json({ alreadyliked: 'User has not yet liked this post' })
+          }
+
+          const removeIndex = post.likes
+            .map(item => item.user.toString())
+            .indexOf(req.user.id)
+          post.likes.splice(removeIndex, 1)
+          post.save().then(post => res.json(post))
+        })
+        .catch(err =>
+          res.status(404).json({ nopostfound: 'No post found with that id' })
+        )
+    })
+  }
+)
+
+/***********************************************
+ *  @route  -->  POST api/posts/comment/:postId
+ *  @desc   -->  Add comment to post
+ *  @access -->  Private
+ */
+router.post(
+  '/comment/:postId',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateCommentInput(req.body)
+    if (!isValid) {
+      return res.status(400).json(errors)
+    }
+
+    Post.findById(req.params.postId)
+      .then(post => {
+        const newComment = {
+          text: req.body.text,
+          name: req.body.name,
+          avatar: req.body.avatar,
+          user: req.user.id
+        }
+
+        post.comments.unshift(newComment)
+        post.save().then(post => res.json(post))
+      })
+      .catch(err =>
+        res.status(404).json({ nopostfound: 'No post found with that id' })
+      )
+  }
+)
+
+/************************************************************
+ *  @route  -->  DELETE api/posts/comment/:postId/:commentId
+ *  @desc   -->  Remove comment from post
+ *  @access -->  Private
+ */
+router.delete(
+  '/comment/:postId/:commentId',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Post.findById(req.params.postId)
+      .then(post => {
+        // Ensure comment exists
+        if (
+          post.comments.filter(
+            comment => comment._id.toString() === req.params.commentId
+          ).length === 0
+        ) {
+          return res
+            .status(404)
+            .json({ nocommentfound: 'No comment found with that id' })
+        }
+
+        const removeIndex = post.comments
+          .map(item => item._id.toString())
+          .indexOf(req.params.commentId)
+        post.comments.splice(removeIndex, 1)
+        post.save().then(post => res.json(post))
+      })
+      .catch(err =>
+        res.status(404).json({ nopostfound: 'No post found with that id' })
+      )
+  }
 )
 
 module.exports = router
---
+++ validation/comment.js
@@ -0,0 +1,17 @@
+const Validator = require('validator')
+const isEmpty = require('./is-empty')
+
+module.exports = function validateCommentInput(data) {
+  let errors = {}
+
+  // Ensure empty strings before Validator gets a hold of it
+  data.text = !isEmpty(data.text) ? data.text : ''
+
+  if (Validator.isEmpty(data.text)) {
+    errors.text = 'Text field is required'
+  } else if (!Validator.isLength(data.text, { min: 10, max: 300 })) {
+    errors.text = 'Text must be between 10 and 300 characters'
+  }
+
+  return { errors, isValid: isEmpty(errors) }
+}
