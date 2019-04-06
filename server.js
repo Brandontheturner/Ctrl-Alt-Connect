78f400e8cb096e3f3e82d80ba60849b3c2cf608e
andyols <andyolson09@gmail.com>
2019-04-06 00:23:03 -0500

    User registration to database

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 
 const app = express()
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 // Hook up mongoURI
 const db = require('./config/keys').mongoURI
