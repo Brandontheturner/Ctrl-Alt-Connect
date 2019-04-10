1584eff909fe4d9852b4c5278ff6501e67117d12
andyols <andyolson09@gmail.com>
2019-04-10 16:41:16 -0500

    working on profile creds sectoin

--- client/src/components/profile/ProfileCreds.js
+++ client/src/components/profile/ProfileCreds.js
@@ -1,62 +1,38 @@
 import React, { Component } from 'react'
 import Moment from 'react-moment'
-import { Grid, List, Header, Segment } from 'semantic-ui-react'
+import { Grid, Item, Header, Segment } from 'semantic-ui-react'
 
 class ProfileCreds extends Component {
   render() {
     const { experience, education } = this.props
+    const experienceItems = experience.map(item => ({
+      childKey: item._id,
+      header: item.company,
+      meta: item.location === '' ? null : item.location,
+      extra: item.description === '' ? null : item.description
+    }))
+    const educationItems = education.map(item => ({
+      childKey: item._id,
+      header: item.school,
+      meta: item.degree,
+      description: item.fieldofstudy,
+      extra: item.description === '' ? null : item.description
+    }))
     return (
       <Grid columns={2}>
+        {/* EXPERIENCE */}
         <Grid.Column>
           <Header content="Experience" color="blue" textAlign="center" />
           <Segment>
-            <List relaxed divided>
-              {experience.map(item => (
-                <List.Item key={item._id}>
-                  <List.Header content={item.company} />
-                  <List.Description>
-                    <Moment format="YYYY/MM/DD">{item.from}</Moment> -
-                    {item.to === null ? (
-                      ' Now'
-                    ) : (
-                      <Moment format=" YYYY/MM/DD">{item.to}</Moment>
-                    )}
-                  </List.Description>
-                  <List.Content
-                    content={item.location === '' ? null : item.location}
-                  />
-                  <List.Content
-                    content={item.description === '' ? null : item.description}
-                  />
-                </List.Item>
-              ))}
-            </List>
+            <Item.Group relaxed divided items={experienceItems} />
           </Segment>
         </Grid.Column>
 
+        {/* EDUCATION */}
         <Grid.Column>
           <Header content="Education" color="blue" textAlign="center" />
           <Segment>
-            <List relaxed divided>
-              {education.map(item => (
-                <List.Item key={item._id}>
-                  <List.Header content={item.school} />
-                  <List.Description>
-                    <Moment format="YYYY/MM/DD">{item.from}</Moment> -
-                    {item.to === null ? (
-                      ' Now'
-                    ) : (
-                      <Moment format=" YYYY/MM/DD">{item.to}</Moment>
-                    )}
-                  </List.Description>
-                  <List.Content content={item.degree} />
-                  <List.Content content={item.fieldofstudy} />
-                  <List.Content
-                    content={item.description === '' ? null : item.description}
-                  />
-                </List.Item>
-              ))}
-            </List>
+            <Item.Group relaxed divided items={educationItems} />
           </Segment>
         </Grid.Column>
       </Grid>
